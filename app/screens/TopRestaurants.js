f6e9a73796dea8e506a28572fdb8806aacdce539
Emanuel Castillo <emanuel.castillo@agence.cl>
2020-05-13 16:39:01 -0400

    Added - module top ranking

--- app/screens/TopRestaurants.js
+++ app/screens/TopRestaurants.js
@@ -1,10 +1,50 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState, useEffect, useRef } from "react";
+import { View } from "react-native";
+import Toast from "react-native-easy-toast";
 
-export default function TopRestaurants() {
+import { firebaseApp } from "../utils/firebase";
+import firebase from "firebase/app";
+import "firebase/firestore";
+import ListTopRestaurants from "../components/Ranking/ListTopRestaurants";
+
+const db = firebase.firestore(firebaseApp);
+
+export default function TopRestaurants(props) {
+    const { navigation } = props;
+
+    const toastRef = useRef();
+
+    const [restaurants, setRestaurants] = useState([]);
+
+    // TODO: select firebase orderby
+    useEffect(() => {
+
+        db.collection("restaurants")
+            .orderBy("rating", "desc")
+            .limit(5)
+            .get()
+                .then((response) => {
+
+                    const restaurantsArr = [];
+
+                    response.forEach(doc => {
+                        const data = doc.data();
+                        data.id = doc.id;
+                        restaurantsArr.push(data);
+                    });
+
+                    setRestaurants(restaurantsArr);
+                })
+
+    }, []);
+
     return (
         <View>
-            <Text>TopRestaurants</Text>
+            <ListTopRestaurants
+                restaurants={restaurants}
+                navigation={navigation}
+            />
+            <Toast ref={toastRef} position="center" opacity={0.9} />
         </View>
     );
 }
